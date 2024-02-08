f26997e73d69bc4fb16c46652765cc5676ac0592
Cécile Tran Kiem <ctrankie@fredhutch.org>
2024-02-07 17:50:43 -0800

    Update figure 1

--- scripts/explore_distribution_cluster_identical_sequences.R
+++ scripts/explore_distribution_cluster_identical_sequences.R
@@ -26,22 +26,32 @@ log_breaks_ratio <- sapply(log(breaks_ratio), FUN = function(curr_log_break){
 })
 
 plt_competition_mutation_transmission <- df_mean_nb_offspring %>% 
-  ggplot(aes(y = log_ratio, x = R0)) +
+  ggplot(aes(x = R0, y = proba_trans_before_mut)) +
   geom_raster(aes(fill = mean_nb_offspring)) +
   geom_contour(aes(z = mean_nb_offspring), breaks = c(0.2, 0.6, 1.0, 1.4, 1.8), col = 'white') +
   geom_text_contour(aes(z = mean_nb_offspring),
                     nudge_x = 0.1, nudge_y = 0.1,
                     col = 'white', label.placer = label_placer_n(1)) +
-  scale_fill_viridis(name = 'Mean number of offsprings\nwith identical genomes', option = 'mako') +
-  scale_y_continuous(name = expression(d[mut]/d[gen]),
-                     breaks = log_breaks_ratio, labels = breaks_ratio,
+  scale_fill_viridis(name = 'Mean number\nof offsprings\nwith identical\ngenomes', option = 'mako') +
+  scale_y_continuous(name = expression(p),
+                     trans = 'logit',
+                     breaks = c(0.05, 0.1, 0.2, 0.5, 0.8, 0.9, 0.95),
                      expand = expansion(mult = c(0., 0.))) +
   scale_x_continuous(name = 'Reproduction number R',
                      expand = expansion(mult = c(0., 0.)))  +
   theme_classic() +
-  theme(axis.line = element_blank())
-  
-plot(plt_competition_mutation_transmission)
+  theme(axis.line = element_blank(),
+        axis.text = element_text(size = 12),
+        axis.title = element_text(size = 12),
+        legend.text = element_text(size = 12),
+        legend.title = element_text(size = 12),
+        legend.position = 'bottom') +
+  guides(fill = guide_colorbar(barwidth = unit(1.7, 'in')))
+
+# pdf('../figures/mean_nb_offspring_identical_sequences.pdf',
+#     height = 3.5, width = 3.0)
+# plot(plt_competition_mutation_transmission)
+# dev.off()  
 
 ##### 2) Cumulative distribution function  
 # for different assumptions regarding the reproduction number R, 
